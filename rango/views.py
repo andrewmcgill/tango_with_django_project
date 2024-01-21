53e0f2d1d5a6026564647bf21412abdcdb190845
Andrew McGill <2762627m@student.gla.ac.uk>
2024-01-21 09:23:30 +0000

    chp4 initial template use

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
+    # pass to template engine as context
+    # boldmessage matches that in template - ANY instance replaced
+    context_dict = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    # rendered reponse returned to client
+    # shortcut function
+    # first parameter is the template we want to use
+    return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
     return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')

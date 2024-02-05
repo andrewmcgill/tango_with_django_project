b4372c6195abf07b715835a7fceccfa9299e3b28
Andrew McGill <2762627m@student.gla.ac.uk>
2024-02-05 15:26:32 +0000

    chp10 complete + tests OK

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
@@ -27,10 +28,16 @@ def index(request):
     # rendered reponse returned to client
     # shortcut function
     # first parameter is the template we want to use
+    visitor_cookie_handler(request)
+    
     return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
     context_dict = {'boldmessage':'This tutorial has been put together by Andrew McGill.'}
+    visitor_cookie_handler(request)
+    
+    context_dict['visits'] = int(request.session['visits'])
+    
     return render(request, 'rango/about.html', context = context_dict)
 
 def show_category(request, category_name_slug):
@@ -176,6 +183,31 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+        
+    request.session['visits'] = visits
+    
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+    
+    
+    
+    
     
     
                 
